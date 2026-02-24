bd7e415824d64e3cf3ed1cb0ca676e4f8e4b3d90
jiawei chen <jiawei.chen25@stud.itsaltoadriatico.it>
2026-02-24 12:05:09 +0100

    ho fatto un aggiornamento

--- main.py
+++ main.py
@@ -3,18 +3,27 @@ file = input("immetere i percorso file: ")
 def robe(file):
     lista1 = []
     punti1 = []
+    parole = []
     with open(file, "r") as fil:
         contenuto = fil.read()
+    #parole = contenuto.split()
+    for x in contenuto.split():
+        if x.isalpha():
+            parole.append(x)
     for x in contenuto:
         if x == " ":
             lista1.append(x)
         elif x == ".":
             punti1.append(x)
-    return lista1, punti1
+    return lista1, punti1, parole
 
-lista1, punti1 = robe(file)
+lista1, punti1, parole = robe(file)
 print("il numero di spazi è:", len(lista1))
 print("il numero dei punti è : ", len(punti1))
+print("I numeri delle parole sono:", len(parole))
+print("La parola più lunga è: ", max(parole, key=len))#serza key=len prende le parole in ordine alfabetico
+print("La parola più corta è : ", min(parole, key=len))
+
 
 
 
